6cb0370b88d4cb965c60dbe03e1a74a1b6255fa8
Leeseungmin <59906532+sammundong@users.noreply.github.com>
2023-01-11 20:52:40 +0900

    1.10
    kl

---
+++ beakjoon/python/1000.py
@@ -0,0 +1,4 @@
+B, A = input().split()
+A=int(A)
+B=int(B)
+print(A+B)
---
+++ beakjoon/python/2588.py
@@ -0,0 +1,10 @@
+A=int(input())
+B=int(input())
+
+print(A * (B % 10))
+
+print(A * (B % 100 // 10))
+
+print(A * (B // 100))
+
+print(A * B)
